e637630cecf5a7c005f7eacd304b237d76afe41e
ravaen1 <tuemail@example.com>
2023-05-14 14:41:19 -0500

    modificandofunciones

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import Header from "./components/Header"
 function App() {
   let [cantidad, setCantidad] = useState(10000);
 
+  function handleChange(e) {
+    console.log(e.target.value)
+  }
+
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
       <input type="range" 
       className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
-      onChange={ e => console.log(e.target.value)}
+      onChange={handleChange}
        />
 
     </div>
