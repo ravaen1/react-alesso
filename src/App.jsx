185819cd162466543d21ce7a262480ef1dcc28c2
ravaen1 <tuemail@example.com>
2023-05-14 14:31:19 -0500

    el primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import Header from "./components/Header"
+
+function App() {
+  let [cantidad, setCantidad] = useState(10000);
+
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+
+      <input type="range" 
+      className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      onChange={ e => console.log(e.target.value)}
+       />
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header(){
+
+    return(
+        <h1 className="tex-4xl font-extrabold text-gray-500 text-center">
+        cuanto <span className="text-indigo-600">dinero </span> necesitas? perro
+      </h1>
+    )
+}
+
+export default Header;
