6a6b92107fb93ea3940e1bd08489a891f73bf955
ravaen1 <tuemail@example.com>
2023-05-15 23:15:27 -0500

    cambios en el header

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,45 @@ import { useState } from "react"
 import Header from "./components/Header"
 
 function App() {
-  let [cantidad, setCantidad] = useState(10000);
+  const [cantidad, setCantidad] = useState(10000);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100  ;
 
   function handleChange(e) {
-    console.log(e.target.value)
+    setCantidad(+e.target.value)
   }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
+      <div className="flex justify-between my-6">
+
+        <button
+        type="button"
+        className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-600"        
+        >-</button>
+      
+
+      <button
+        type="button"
+        className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-600"        
+        >+</button>
+
+      </div>
+
       <input type="range" 
       className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
       onChange={handleChange}
+      min = {MIN}
+      max = {MAX}
+      step = {STEP}
+      value = {cantidad}
        />
+
+       <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
 
     </div>
     
